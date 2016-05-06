f1138ae2473a2ba27eccb4a089f630310b5d79c3
Vadik <ivadik2323@gmail.com>
2016-05-07 05:52:28 +1000

    delete MemoryPool from allocator

--- allocator.h
+++ allocator.h
@@ -12,48 +12,27 @@ struct Allocator {
 	typedef const value_type& const_reference;
 
 	Allocator()
-	{
-		//_pool = &MemoryPool(1024);
-	}
-
-	Allocator(MemoryPool* pool) 
-	{
-		_pool = pool;
-	}
+	{	}
 
 	~Allocator() throw() {}
 
 	template <typename U> 
 	Allocator(const Allocator<U>& other) 
-	{
-		_pool = other._pool;
-	}
+	{	}
 
 	value_type* allocate(size_type n)
-	{
-		//return static_cast<pointer>(operator new(n * sizeof(T)));
-		return (pointer)_pool->Allocate(sizeof(value_type) * n);
-	}
+	{  return new value_type[n]; }
 
 	void deallocate(value_type* p, size_type n)
-	{
-		//operator delete(p);
-		_pool->Deallocate(p);
-	}
+	{ delete[] p; }
 
 	template<class U, class... Args>
-	void construct(U* p, Args&&... args) {
-		new((void *)p) U(std::forward<Args>(args)...);
-	}
+	void construct(U* p, Args&&... args) 
+	{ new((void *)p) U(std::forward<Args>(args)...); }
 
 	template<class U>
-	void destroy(U* p) {
-		p->~U();
-	}
-
-	MemoryPool* _pool;
-
-private:
+	void destroy(U* p) 
+	{ p->~U(); }
 };
 
 template <class T, class U>
